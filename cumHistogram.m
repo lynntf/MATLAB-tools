5ff8bd6525b6bbc3bb9c61db3c9491cf86d1def0
lynntf <32374143+lynntf@users.noreply.github.com>
2019-11-25 23:44:02 -0600

    Update cumHistogram.m
    Fixed function name, added comments, removed hard-coded zero (sampling started at zero).

--- cumHistogram.m
+++ cumHistogram.m
@@ -1,5 +1,8 @@
-function [cum_h,t_h] = cum_histogram(in,t)
-
+function [cum_h,t_h] = cumHistogram(in,t)
+% Function to get cumulative histogram of some input
+% If t is just an integer, will use that many equally spaced points to measure
+% otherwise, the sample points can be specified with the input (non-linear 
+% sampling is possible).
 if length(t) > 1
     N = length(t);
     cum_h = zeros(length(N),1);
@@ -7,7 +10,7 @@ if length(t) > 1
 else
     N = t+1;
     cum_h = zeros(N,1);
-    t_h = linspace(0,max(in(:)),N)';
+    t_h = linspace(min(in(:)),max(in(:)),N)';
 end
 
 for ii = 1:N
