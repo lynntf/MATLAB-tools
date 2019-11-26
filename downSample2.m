d7e5e7ca35394be094941aafc4d771b04c7e4b37
lynntf <thomaslynn2021@u.northwestern.edu>
2019-11-26 13:13:52 -0600

    Added warnings Added contingency for odd dimensions and a warning

--- downSample2.m
+++ downSample2.m
@@ -1,9 +1,19 @@
-function x = downSample2(x,n)
-% Function downSample2 takes a 2D array x and averages four neighbors into a single entry.
-if ~exist('n','var') % parameter n specifies the number of times to down-sample
-    n = 1;
+function x = downSample2(x,N)
+% Function downSample2 takes a 2D array x and averages four neighbors into 
+% a single entry.
+if ~exist('N','var') % parameter N specifies the number of times to down-sample
+    N = 1;
 end
-for ii = 1:n
+for ii = 1:N
+    [m,n] = size(x);
+    if mod(m, 2)
+        x = [x;x(end,:)];
+        fprintf('Warning: first dimension not even\n');
+    end
+    if mod(n, 2)
+        x = [x,x(:,end)];
+        fprintf('Warning: second dimension not even\n');
+    end
     x = (x(1:2:size(x,1),1:2:size(x,2)) + x(1:2:size(x,1),2:2:size(x,2)) + ...
         x(2:2:size(x,1),1:2:size(x,2)) + x(2:2:size(x,1),2:2:size(x,2)))/4;
 end
